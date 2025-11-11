a62bb6ae03518a595470437797b522481f66663d
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-11 10:43:26 +0300

    first commit

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getMessages } = require('../controllers/messages');
+const { getMessages, deleteMessage } = require('../controllers/messages');
 const { authMiddleware } = require('../middleware/auth');
 
 // Protected route to get messages
 router.get('/', authMiddleware, getMessages);
+
+// Protected route to delete a message
+router.delete('/:messageId', authMiddleware, deleteMessage);
 
 module.exports = router;
